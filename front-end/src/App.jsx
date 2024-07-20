73f08f9bc29c2db68b8505c116795d2d22085c6c
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-07-20 14:06:43 -0400

    .

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
-import Categoria from './pages/categoria/Categoria';
-import Lugares from './pages/lugares/Lugares';
+import Categoria from "./pages/categoria/Categoria";
+import Lugares from "./pages/lugares/Lugares";
 import LugarPage from "./pages/categoria/detalleCategoria/LugarPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/categoria" element={<Lugares/>} />
-      <Route path="/categoria/:category" element={<Lugares/>} />
-       <Route path="/categoria/lugar/:id" element={<LugarPage />} />
+      <Route path="/categoria" element={<Lugares />} />
+      <Route path="/categoria/:category" element={<Lugares />} />
+      <Route path="/categoria/lugar/:id" element={<LugarPage />} />
     </Routes>
   );
 }
